8587fbcd5abc3cdb3480f0db6419333b706a78ab
Mohit Gudadhe <mohitgudadhe2803@gmail.com>
2026-05-02 01:49:15 +0530

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
+        int start = 1;
+        int end = Arrays.stream(piles).max().getAsInt();
+        int ans = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            long total = 0;
+            for (int pile : piles ){
+                total += Math.ceil( (double)pile / mid);
+            }
+            if (total <= h){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
