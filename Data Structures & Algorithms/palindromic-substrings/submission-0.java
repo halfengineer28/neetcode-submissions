4607e60f1cd27e4a20937f2572129487484fe20f
Mohit Gudadhe <mohitgudadhe2803@gmail.com>
2026-05-19 01:40:50 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countSubstrings(String s) {
+
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            count += countPalindrome(s, i, i);
+            count += countPalindrome(s, i, i+1);
+           
+        }
+        return count;
+
+
+        
+    }
+    public int countPalindrome(String s, int left , int right){
+        int count = 0;
+
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            
+            left--;
+            right++;
+            count++;
+        }
+        
+        return count;
+    }
+}
