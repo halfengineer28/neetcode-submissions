42e5f429a2e1dd1c5917c71242f1d16423b21ae8
halfengineer28 <mohitgudadhe2803@gmail.com>
2026-09-08 00:36:21 +0530

    Update: permutation-string - submission-0

--- Data Structures & Algorithms/permutation-string/submission-0.java
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -1,36 +1,28 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length() > s2.length()){
-            return false;
-        }
-        
-       Map<Character , Integer> count1 = new HashMap<>();
-       for(char c : s1.toCharArray()){
-        count1.put(c, count1.getOrDefault(c, 0) +1);
 
-       }
-       int need = count1.size();
-       for(int i = 0; i<s2.length(); i++){
-        Map<Character, Integer> count2 = new HashMap<>();
-        int cur = 0;
-        for(int j = i; j<s2.length(); j++){
-            char c = s2.charAt(j);
-            count2.put(c, count2.getOrDefault(c, 0) + 1);
+        int left = 0;
+        int right = s1.length()-1;
+        char [] str = s1.toCharArray();
+        Arrays.sort(str);
+        String have = new String(str);
 
-            if(count1.getOrDefault(c, 0) < count2.get(c)){
-                break;
-            }
-
-            if(count1.getOrDefault(c, 0) == count2.get(c)){
-                cur++;
-            }
-
-            if(cur == need){
+        while (right < s2.length()){
+            String need = s2.substring(left, right+1);
+            char [] sorted = need.toCharArray();
+            Arrays.sort(sorted);
+            String exists = new String(sorted);
+            if (have.equals(exists)){
                 return true;
+            }else{
+                left++;
+                right++;
             }
+
+
         }
-       }
-       return false;
-        
+
+       
+       return false; 
     }
 }
